94579cd58a945e7b72eca1e05097c38bf63c79f3
KachusovStepan <stepanurfu@gmail.com>
2021-05-08 23:05:41 +0500

    added xml docs for InMemoryDeckRepository

--- DeckApi/Services/InMemoryDeckRepository.cs
+++ DeckApi/Services/InMemoryDeckRepository.cs
@@ -13,6 +13,11 @@ namespace DeckApi.Services
         public readonly string ShuffleType;
         private ShuffleCards<Card> shuffle;
 
+        /// <summary>
+        /// Creates InMemoryDeckRepository with specified shuffle algorithm 
+        /// </summary>
+        /// <param name="shuffleType">shuffle algorithm: "Simple" or "Manual"</param>
+        /// <exception cref="ArgumentException">Throws an exception if the algorithm is not recognized</exception>
         public InMemoryDeckRepository(string shuffleType = "Simple")
         {
             ShuffleType = shuffleType;
@@ -22,7 +27,7 @@ namespace DeckApi.Services
                     shuffle = Shuffler.SimpleShuffle;
                     break;
                 case "Manual":
-                    shuffle = Shuffler.SimpleShuffle;
+                    shuffle = Shuffler.ManualShuffle;
                     break;
                 default:
                     throw new ArgumentException($"Unrecognized Shuffle Type: {shuffleType}");
@@ -31,21 +36,40 @@ namespace DeckApi.Services
             deckDict = new ConcurrentDictionary<string, Deck>();
         }
 
+        /// <summary>
+        /// Creates new deck with specified name
+        /// </summary>
+        /// <param name="name">New deck name</param>
+        /// <returns>True if was successfully created</returns>
         public async Task<bool> CreateNewDeck(string name)
         {
             return deckDict.TryAdd(name, new Deck());
         }
 
+        /// <summary>
+        /// Deletes existing deck with specified name
+        /// </summary>
+        /// <param name="name">Deck name to be deleted</param>
+        /// <returns>True if was successfully deleted</returns>
         public async Task<bool> DeleteDeck(string name)
         {
             return deckDict.TryRemove(name, out Deck removedDeck);
         }
 
+        /// <summary>
+        /// Gets names of existing decks (not ordered)
+        /// </summary>
+        /// <returns>List of existing deck names</returns>
         public async Task<List<string>> GetDeckNames()
         {
             return deckDict.Keys.ToList();
         }
 
+        /// <summary>
+        /// Shuffles deck with specified name
+        /// </summary>
+        /// <param name="name">The name of the deck shuffling</param>
+        /// <returns>List of existing deck shuffled</returns>
         public async Task<bool> ShuffleDeck(string name)
         {
             if (deckDict.TryGetValue(name, out Deck deck))
@@ -57,6 +81,11 @@ namespace DeckApi.Services
             return false;
         }
 
+        /// <summary>
+        /// Gets deck by its name
+        /// </summary>
+        /// <param name="name">Query deck name</param>
+        /// <returns>Queried deck</returns>
         public async Task<Deck> GetDeck(string name)
         {
             if (deckDict.TryGetValue(name, out Deck deck))
